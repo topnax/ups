9b6083433352aaaee33a1246260f90214bcf4820
Stanislav Král <stanislav.kral3@gmail.com>
2019-10-05 19:12:25 +0200

    Progress...

--- sp/server/main.go
+++ sp/server/main.go
@@ -5,28 +5,48 @@ import (
 	"ups/sp/server/model"
 )
 
-
 func main() {
 
 	//
 	desk := model.GetDesk()
 
 	desk.Print()
-	desk.SetAt("A", 0,1)
-	desk.SetAt("B", 2,1)
-	desk.SetAt("B", 2,1)
-	desk.SetAt("CH", 14,14)
+	desk.SetAt("A", 0, 1)
+	desk.SetAt("B", 2, 1)
+	desk.SetAt("B", 2, 1)
+	desk.SetAt("CH", 14, 14)
 	fmt.Println("Out of bounds")
-	desk.SetAt("Z", 15,15)
-	desk.SetAt("Z", 14,15)
-	desk.SetAt("Z", 15,14)
-	desk.SetAt("CH", 14,14)
-	desk.SetAt("131", 13,13)
-	desk.SetAt("1", 12,13)
-	desk.SetAt("Z", -1,15)
-	desk.SetAt("Z", 15,-1)
-	desk.SetAt("Z", 15,-1)
+	desk.SetAt("K", 3, 3)
+	desk.SetAt("R", 3, 4)
+	desk.SetAt("I", 3, 5)
+	desk.SetAt("S", 3, 6)
+
+	desk.SetAt("K", 2, 4)
+
+	desk.SetAt("O", 4, 4)
+	desk.SetAt("S", 5, 4)
+
+	desk.SetAt("Z", 14, 15)
+	desk.SetAt("Z", 15, 14)
+	desk.SetAt("CH", 14, 14)
+	desk.SetAt("131", 13, 13)
+	desk.SetAt("1", 12, 13)
+	desk.SetAt("Z", -1, 15)
+	desk.SetAt("Z", 15, -1)
+	desk.SetAt("Z", 15, -1)
+
+	error := desk.SetWordAt(3, 3, 3, 6, 1)
+	error2 := desk.SetWordAt(2, 4, 5, 4, 1)
+
 	//desk.SetAt("B", 16,15)
 	//desk.SetAt("B", 15,15)
 	desk.Print()
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	if error2 != nil {
+		fmt.Println(error2)
+	}
 }
--- sp/server/model/desk.go
+++ sp/server/model/desk.go
@@ -21,18 +21,20 @@ type Letter struct {
 	Set   bool
 	Type  int
 	Value string
-	Used int
+	Used  int
 }
 
 type Word struct {
-	Start [2] int
-	End [2] int
+	Start    [2]int
+	End      [2]int
 	PlayerID int
+	Content  string
+	Points   int
 }
 
 type Desk struct {
-	Letters [deskSize][deskSize] Letter
-	Words [] Word
+	Letters [deskSize][deskSize]Letter
+	Words   []Word
 }
 
 type KrisKrosDesk interface {
@@ -49,7 +51,7 @@ func (desk *Desk) Create() {
 			letters[row][column] = Letter{
 				Set:   false,
 				Type:  0,
-				Used: 0,
+				Used:  0,
 				Value: "",
 			}
 		}
@@ -82,34 +84,51 @@ func (desk *Desk) SetWordAt(rowStart int, columnStart int, rowEnd int, columnEnd
 		return errors.New("a word mustn't be diagonal")
 	}
 
+	var content []string
+
 	if rowStart == rowEnd {
-		for column := columnStart; column < columnEnd; column++ {
+		for column := columnStart; column < columnEnd+1; column++ {
 			if desk.Letters[rowStart][column].Used >= 2 {
 				return errors.New("a word cannot be used more than two times ")
+			} else if !desk.Letters[rowStart][column].Set {
+				return errors.New("a letter has to be set everywhere inside the range")
 			}
 		}
 
-		for column := columnStart; column < columnEnd; column++ {
+		if desk.Letters[rowStart][columnStart-1].Set || desk.Letters[rowStart][columnEnd+1].Set {
+			return errors.New("no letter can't be after or before the word")
+		}
+
+		for column := columnStart; column < columnEnd+1; column++ {
+			content = append(content, desk.Letters[rowStart][column].Value)
 			desk.Letters[rowStart][column].Used++
 		}
 	}
 
 	if columnStart == columnEnd {
-		for row := rowStart; row < rowEnd; row++ {
+		for row := rowStart; row < rowEnd+1; row++ {
 			if desk.Letters[row][columnStart].Used >= 2 {
 				return errors.New("a word cannot be used more than two times ")
+			} else if !desk.Letters[row][columnStart].Set {
+				return errors.New("a letter has to be set everywhere inside the range")
 			}
 		}
 
-		for row := rowStart; row < rowEnd; row++ {
+		if desk.Letters[rowEnd+1][columnStart].Set || desk.Letters[rowStart-1][columnStart].Set {
+			return errors.New("no letter can't be after or before the word")
+		}
+
+		for row := rowStart; row < rowEnd+1; row++ {
+			content = append(content, desk.Letters[row][columnStart].Value)
 			desk.Letters[row][columnStart].Used++
 		}
 	}
 
 	desk.Words = append(desk.Words, Word{
 		Start:    [2]int{rowStart, columnStart},
-		End:      [2]int{rowEnd, columnStart},
+		End:      [2]int{rowEnd, columnEnd},
 		PlayerID: playerId,
+		Content:  strings.Join(content[:], ""),
 	})
 
 	return nil
@@ -125,7 +144,13 @@ func isNumber(s string) bool {
 }
 
 func (desk Desk) Print() {
+	fmt.Println(" 123456789012345")
 	for row := 0; row < deskSize; row++ {
+		if row < 9 {
+			fmt.Print(row + 1)
+		} else {
+			fmt.Print(row - 9)
+		}
 		for column := 0; column < deskSize; column++ {
 			if desk.Letters[row][column].Set {
 				fmt.Print(strings.ToUpper(desk.Letters[row][column].Value))
@@ -134,8 +159,15 @@ func (desk Desk) Print() {
 			}
 		}
 		fmt.Print("\n")
-
 	}
+
+	if len(desk.Words) > 0 {
+		fmt.Println("Words:")
+		for _, word := range desk.Words {
+			fmt.Println(word.Content, "by user", word.PlayerID, "at", word.Start[0], word.Start[1], "to", word.End[0], word.End[1])
+		}
+	}
+
 }
 
 func GetDesk() Desk {
