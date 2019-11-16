f1402276cbc90eff62afd5e5dffbe18b1628e5e7
Stanislav Král <stanislav.kral3@gmail.com>
2019-11-16 20:43:04 +0100

    Klaxon implementation progress, added parsing JSON to ApplicationMessage

--- sp/client/src/main/kotlin/networking/messages/ApplicationMessage.kt
+++ sp/client/src/main/kotlin/networking/messages/ApplicationMessage.kt
@@ -1,27 +1,69 @@
 package networking.messages
 
-import com.beust.klaxon.FieldRenamer
-import com.beust.klaxon.Json
-import com.beust.klaxon.Klaxon
+import com.beust.klaxon.*
+import kotlin.reflect.KClass
 
+class MessageTypeAdapter: TypeAdapter<ApplicationMessage> {
+    override fun classFor(type: Any): KClass<out ApplicationMessage> = when(type as Int) {
+        JoinLobbyMessage(0, "").type -> JoinLobbyMessage::class
+        PlayerJoinedLobby(0, "").type -> PlayerJoinedLobby::class
+        else -> throw IllegalArgumentException("Unknown type: $type")
+    }
+}
+
+@TypeFor(field = "type", adapter = MessageTypeAdapter::class)
 abstract class ApplicationMessage(@Json(ignored = true) val type: Int) {
 
     companion object {
-        private val renamer = object: FieldRenamer {
+        val renamer = object : FieldRenamer {
             override fun toJson(fieldName: String) = FieldRenamer.camelToUnderscores(fieldName)
             override fun fromJson(fieldName: String) = FieldRenamer.underscoreToCamel(fieldName)
         }
+
+        inline fun <reified T> fromJson(json: String): T? where T : ApplicationMessage {
+            return Klaxon().fieldRenamer(renamer).parse<T>(json)
+        }
+
+
+        fun fromJsonNew(json: String) : ApplicationMessage? {
+            return Klaxon().fieldRenamer(renamer).parse<ApplicationMessage>(json)
+        }
+
     }
 
     fun toJson(): String {
         return Klaxon().fieldRenamer(renamer).toJsonString(this)
     }
+    inline fun <reified T> fromJson(json: String): T? where T : ApplicationMessage {
+        return Klaxon().fieldRenamer(renamer).parse<T>(json)
+    }
+
 }
 
-data class JoinLobbyMessage(val lobbyId: Int, val playerName: String, val foo: Foo) : ApplicationMessage(1)
+data class JoinLobbyMessage(val lobbyId: Int, val playerName: String) : ApplicationMessage(1)
 
-data class Foo(val bar: Int, val baz: String)
+data class PlayerJoinedLobby(val playerId: Int, val playerName: String) : ApplicationMessage(2)
 
 fun main() {
-    println(JoinLobbyMessage(12, "Topnax", Foo(42, "This is Foo")).toJson())
+    val json = """
+        {
+            "lobby_id": 10,
+            "player_name": "Topinkos"
+        }
+    """
+
+    val msg = ApplicationMessage.fromJsonNew(json)
+
+//    Klaxon().converter()
+
+    msg?.let {
+        if (it is JoinLobbyMessage) {
+            println(it.playerName)
+            println(it.lobbyId)
+        } else if (it is PlayerJoinedLobby) {
+            println("pjl")
+            println(it.playerName)
+            println(it.lobbyId)
+        }
+    }
 }
--- sp/client/src/main/kotlin/networking/reader/SimpleMessageReader.kt
+++ sp/client/src/main/kotlin/networking/reader/SimpleMessageReader.kt
@@ -1,9 +1,31 @@
 package networking.reader
 
+import networking.messages.ApplicationMessage
+import networking.messages.JoinLobbyMessage
+import networking.messages.PlayerJoinedLobby
 import networking.receiver.Message
 
 class SimpleMessageReader : MessageReader {
-    override fun read(message: Message) {
 
+    private val messageTypes = hashMapOf<Int, Class<out ApplicationMessage>>()
+    private val messageTypesX = hashMapOf<Int, (String) -> ApplicationMessage >()
+
+    init {
+        register(JoinLobbyMessage(0,""))
+        register(PlayerJoinedLobby(0,""))
+    }
+
+    private fun register(message: ApplicationMessage) {
+        messageTypes.put(message.type, message.javaClass)
+        messageTypesX.put(message.type, {str: String -> return ApplicationMessage.fromJson<>()message.javaClass} )
+    }
+
+    override fun read(message: Message) {
+        val messageType = messageTypes[message.type]
+        val messageTypeX: ApplicationMessage? = messageTypesX[message.type]
+        if (messageTypeX != null) {
+//            ApplicationMessage.fromJson<messageType>(message.content)
+            messageTypeX.fromJson<ApplicationMessage>()
+        }
     }
 }
